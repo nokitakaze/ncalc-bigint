56ef6d955a948699893d98fa4c04da08ba30468c
Nokita Kaze <admin@kanaria.ru>
2023-08-02 00:35:12 +0300

    * Trivial test for BigInteger

---
+++ test/NCalc.Tests/BigIntTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using Xunit;
+
+namespace NCalc.Tests;
+
+public class BigIntTest
+{
+    [Fact]
+    public void TrivialTest()
+    {
+        var e = new NCalc.Expression("a + b");
+        e.EvaluateParameter += delegate(string name, ParameterArgs args)
+        {
+            args.Result = name switch
+            {
+                "a" => new BigInteger(14),
+                "b" => new BigInteger(88),
+                _ => args.Result
+            };
+        };
+
+        object val = e.Evaluate();
+        Assert.Equal(new BigInteger(14 + 88), val);
+    }
+}
